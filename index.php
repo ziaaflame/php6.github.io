a7e5844a25bc2de2ae4b082b6dfa670a3e094534
ziaaflame <38154276+ziaaflame@users.noreply.github.com>
2018-07-26 12:51:19 +0600

    php6

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+    $month = 'May2018';
+    
+    if ($month === 'May2018'){
+        echo 'This month is Ramadan';
+        echo '<br/>';
+    }
+    
+        
+    $name = 'Tarin';
+
+    if ($name === 'Samira'){
+        echo ' She is our Student';
+    } else {
+        echo ' she is our Teacher';
+    }
+?>
